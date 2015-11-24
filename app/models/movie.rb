76abb99c3bf538f6533b9e95d4b36066391ea0d1
anne <richardson.ae@gmail.com>
2015-11-24 16:22:45 -0600

    created user_movies join table and supporting files

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ActiveRecord::Base
   has_many :reviews, as: :reviewable
 
+  has_many :user_movies
+  has_many :users, through: :user_movies
+
   RATING = ["G", "PG", "PG-13", "NC-17", "R", "X"]
   
 end #Movie
